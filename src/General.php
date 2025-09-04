9e6a50a957d1fb2d607dc6555e3e62665ad0e956
Junior Fontenele <github@juniorfontenele.com.br>
2025-09-04 19:40:57 -0300

    Refactor General class for improved readability and consistency in method definitions

--- src/General.php
+++ src/General.php
@@ -1,62 +1,66 @@
 <?php
+
 namespace JuniorFontenele\QualitorWS;
 
 /**
  * Class for General Webservice
  */
-class General extends QualitorWS {
+class General extends QualitorWS
+{
 
-  /**
-   * Class for General Webservice
-   *
-   * @param string $url Qualitor Webservice Base URL
-   * @param string $user Qualitor username
-   * @param string $pass Qualitor password
-   * @param integer $company_id Qualitor company id
-   */
-  public function __construct(string $url, string $user, string $pass, int $company_id) {
-    parent::__construct($url . '/services/General/WSGeneral.wsdl', $user, $pass, $company_id);
-  }
-
-  /**
-   * @param integer $ticket_id
-   * @param integer $additionalinfo_id
-   * @return bool|array
-   */
-  public function getTicketAdditionalInformation(int $ticket_id, int $additionalinfo_id = null) {
-    $query = "SELECT * FROM hd_chamadoinformacaoadicional WHERE cdchamado={$ticket_id}";
-    $additionalInfos = $this->execute('getSQLQueryResult', ['dsquery' => $query]);
-    if (!count($additionalInfos)) {
-      return false;
+    /**
+     * Class for General Webservice
+     *
+     * @param string $url Qualitor Webservice Base URL
+     * @param string $user Qualitor username
+     * @param string $pass Qualitor password
+     * @param integer $company_id Qualitor company id
+     */
+    public function __construct(string $url, string $user, string $pass, int $company_id)
+    {
+        parent::__construct($url . '/services/General/WSGeneral.wsdl', $user, $pass, $company_id);
     }
-    $return = [];
-    foreach ($additionalInfos as $info) {
-      if (!is_array($info)) {
-        $return[$additionalInfos['cdtipoinformacaoadicional']] = $additionalInfos['dsinformacao'] != '' ? trim($additionalInfos['dsinformacao']) : trim($additionalInfos['vlinformacaoadicional']);
-        break;
-      }
-      else {
-        if (is_array($info['dsinformacao'])) {
-          break;
+
+    /**
+     * @param integer $ticket_id
+     * @param ?integer $additionalinfo_id
+     * @return bool|array
+     */
+    public function getTicketAdditionalInformation(int $ticket_id, ?int $additionalinfo_id = null)
+    {
+        $query = "SELECT * FROM hd_chamadoinformacaoadicional WHERE cdchamado={$ticket_id}";
+        $additionalInfos = $this->execute('getSQLQueryResult', ['dsquery' => $query]);
+        if (!count($additionalInfos)) {
+            return false;
         }
-        $return[$info['cdtipoinformacaoadicional']] = $info['dsinformacao'] != '' ? trim($info['dsinformacao']) : trim($info['vlinformacaoadicional']);
-      }
+        $return = [];
+        foreach ($additionalInfos as $info) {
+            if (!is_array($info)) {
+                $return[$additionalInfos['cdtipoinformacaoadicional']] = $additionalInfos['dsinformacao'] != '' ? trim($additionalInfos['dsinformacao']) : trim($additionalInfos['vlinformacaoadicional']);
+                break;
+            } else {
+                if (is_array($info['dsinformacao'])) {
+                    break;
+                }
+                $return[$info['cdtipoinformacaoadicional']] = $info['dsinformacao'] != '' ? trim($info['dsinformacao']) : trim($info['vlinformacaoadicional']);
+            }
+        }
+        if ($additionalinfo_id) {
+            if (!array_key_exists($additionalinfo_id, $return)) {
+                return false;
+            }
+            return $return[$additionalinfo_id];
+        }
+        return $return;
     }
-    if ($additionalinfo_id) {
-      if (!array_key_exists($additionalinfo_id, $return)) {
-        return false;
-      }
-      return $return[$additionalinfo_id];
-    }
-    return $return;
-  }
 
-  /**
-   * @param string $query
-   * @return array
-   */
-  public function getSQLQueryResult(string $query): array {
-    $result = $this->execute('getSQLQueryResult', ['dsquery' => $query]);
-    return $result;
-  }
+    /**
+     * @param string $query
+     * @return array
+     */
+    public function getSQLQueryResult(string $query): array
+    {
+        $result = $this->execute('getSQLQueryResult', ['dsquery' => $query]);
+        return $result;
+    }
 }
